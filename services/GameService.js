1e30fae491cd3d9b2870cbb26b54c9d92078fd81
Julian Yañez <julianfidel06@gmail.com>
2025-09-22 13:30:04 -0300

    add GameService

--- services/GameService.js
+++ services/GameService.js
@@ -5,7 +5,6 @@ class GameService extends EventTarget {
     this._games = [];
   }
 
-<<<<<<< HEAD
   setGames(games) {
     games.map((game) => {
       game.rating = parseInt(game.rating * 1015 );
@@ -14,8 +13,6 @@ class GameService extends EventTarget {
     this.dispatchEvent(new Event("change"));
   }
 
-=======
->>>>>>> 95674d9f441885fc473115c2d1a32ba7926f268a
   getGames() {
     return this._games;
   }
@@ -24,32 +21,14 @@ class GameService extends EventTarget {
     let sorted = this._games.sort((a, b) => {
       return b.rating - a.rating
     });
-<<<<<<< HEAD
     return sorted.slice(0, 15);
-=======
-    console.log(sorted);
-    return sorted.slice(0, 10);
->>>>>>> 95674d9f441885fc473115c2d1a32ba7926f268a
   }
 
   getNewest() {
     let sorted = this._games.sort((a, b) => {
       return new Date(b.released) - new Date(a.released);
     });
-<<<<<<< HEAD
     return sorted.slice(0, 15);
-=======
-    console.log(sorted);
-    return sorted.slice(0, 10);
-  }
-
-  setGames(games) {
-    games.map((game) => {
-      game.rating = parseInt(game.rating * 1015 );
-    });
-    this._games = games;
-    this.dispatchEvent(new Event("change"));
->>>>>>> 95674d9f441885fc473115c2d1a32ba7926f268a
   }
 }
 
