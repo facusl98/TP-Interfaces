dce1a23f02b109b3227c57c0292c0815794cfbc9
Julian Yañez <julianfidel06@gmail.com>
2025-09-26 18:35:27 -0300

    bug fixs

--- components/Common/Header/HeaderComponent.js
+++ components/Common/Header/HeaderComponent.js
@@ -1,31 +1,73 @@
+import { gameService } from "../../../services/GameService.js";
 import { BaseComponent } from "../../BaseComponent.js";
 
 class HeaderComponent extends BaseComponent {
   constructor() {
     super()
+    this._games = [];
   }
 
-  connectedCallback() {
-    this.render();
+  async connectedCallback() {
+    await import("../CustomIcon/CustomIcon.js");
+    await this.render();
+    this._setupEvents();
   }
 
   async render() {
     await this._attachCSS(import.meta.url);
     this.shadowRoot.innerHTML += `
-      <div>
+      <div class = "logo-container">
         <a href="#browse">
         <img src="/assets/images/Logo.png" alt= "Logo"></img>
         </a>
       </div>
-      <div>
-        <input class = "search-header">
-        <custom-icon icon="/assets/icons/common/Search.svg" size="20px">
+      <div class="search-container">
+        <input class ="search-header" id="search-input">
+        <custom-icon icon="/assets/icons/common/Search.svg" size="20px"></custom-icon>
+        <div id="suggestions" class="suggestions" style="display: none;"></div>
       </div>
       <div>
-        <button class= "user-btn"><img src="/assets/images/UserIcon.png"></img></button>
+        <button class="user-btn"><img src="/assets/icons/common/Menu.svg"></img></button>
       </div>
 
     `;
+  }
+
+  _setupEvents() {
+    const input = this.shadowRoot.querySelector("#search-input");
+    const suggestions = this.shadowRoot.querySelector("#suggestions");
+
+    input.addEventListener("keyup", () => {
+
+      if(input.value !== "") {
+        this._showSuggestions(input.value);
+      } else {
+        suggestions.style.display = "none";
+      }
+    });
+
+    input.addEventListener("blur", () => {
+      suggestions.style.display = "none";
+    })
+
+    
+  }
+
+  _showSuggestions(query) {
+    const suggestions = this.shadowRoot.querySelector("#suggestions");
+
+    this._games = gameService.getBy(query, "").slice(0, 3);
+
+    suggestions.innerHTML = this._games.map(game => `
+      <a href="#game/${game.id}"><div class="suggestion-item">${game.name}</div></a>
+    `).join("") + `
+    <div class= "suggestion-item advanced">
+    <a href="#search"><custom-icon icon="/assets/icons/common/Filter.svg" size="15px"></custom-icon>
+    Advanced Search</a>
+
+    </div>
+    `;
+    suggestions.style.display = "block";
   }
 }
 
