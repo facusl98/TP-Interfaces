02f2d13e2b644bc0e1047b7d11834c6f391ec30f
Julian Yañez <julianfidel06@gmail.com>
2025-09-27 23:24:36 -0300

    HTML login and register finshed

--- components/Login/LoginPage.js
+++ components/Login/LoginPage.js
@@ -12,7 +12,38 @@ class LoginPage extends BaseComponent {
   async render() {
     await this._attachCSS(import.meta.url);
     this.shadowRoot.innerHTML += `
-      <h1>Login Page</h1>
+
+      <div class="login-container">
+        <h2>Welcome back</h2>
+        <form method="POST" id="login-form">
+
+          <label for="input-email">Email</label>
+          <input type="email" name="email" id="input-email" required>
+
+          <label for="input-password">Password</label>
+          <input type="password" name="password" id="input-password" required>
+
+          <div class="reCaptcha">
+
+          </div>
+          <a href="#register">First time here? Sign up</a>
+          <button id="login-btn" type="submit">
+            <custom-icon icon="/assets/icons/common/Signin.svg" size="30px"></custom-icon>
+            Sign in
+          </button>
+
+          <button id="google-btn" type="button">
+            <custom-icon icon="/assets/icons/socials/GoogleOriginal.svg" size="35px"></custom-icon>
+            Sign in with Google
+          </button>
+
+          <button id="fcb-btn" type="button">
+            <custom-icon icon="/assets/icons/socials/FacebookOriginal.svg" size="35px"></custom-icon>
+            Sign in with Facebook
+          </button>
+        </form>
+    </div>
+
     `;
 }
 }
--- components/Register/RegisterPage.js
+++ components/Register/RegisterPage.js
@@ -12,7 +12,52 @@ class RegisterPage extends BaseComponent {
   render() {
     this._attachCSS(import.meta.url);
     this.shadowRoot.innerHTML += `
-    <h1>Register Page</h1>
+      <div class="register-container">
+        <h2>New here? Sign up</h2>
+        <form id="register-form" method="POST">
+          <label for="input-email">Email</label>
+          <input type="email" name="email" id="input-email" required>
+
+          <label for="input-name">Full name</label>
+          <input type="text" name="fullName" id="input-name" required>
+
+          <label for="input-age">Age</label>
+          <input type="number" name="age" id="input-age" required>
+
+          <label for="input-username">Username</label>
+          <input type="text" name="username" id="input-username">
+
+          <label for="input-password">Password</label>
+          <input type="password" name="password" id="input-password" required>
+
+          <label for="input-repeatPass">Repeat password</label>
+          <input type="password" name="repeat-password" id="input-repeatPass" required>
+
+          <label for="bio">Bio</label>
+          <textarea name="bio" id="bio"></textarea>
+
+          <div class="reCaptcha">
+              
+          </div>
+
+          <a href="#login">Already have an account? Sign in</a>
+
+          <button type="submit" id="register-btn">
+            <custom-icon icon="/assets/icons/common/SignUp.svg" size="30px"></custom-icon>
+            Sign up
+          </button>
+
+          <button id="google-btn" type="button">
+            <custom-icon icon="/assets/icons/socials/GoogleOriginal.svg" size="35px"></custom-icon>
+            Sign up with Google
+          </button>
+
+          <button id="fcb-btn" type="button">
+            <custom-icon icon="/assets/icons/socials/FacebookOriginal.svg" size="35px"></custom-icon>
+            Sign up with Facebook
+          </button>
+        </form>
+      </div>
     `;
   }
 }
