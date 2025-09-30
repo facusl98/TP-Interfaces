2ab4b5bedaa8ffd227e2eb43620f37aef00bc007
Facundo Castro <facucastrosl@gmail.com>
2025-09-30 13:25:32 -0300

    Game page work in progress

--- components/Game/GamePage.js
+++ components/Game/GamePage.js
@@ -13,7 +13,21 @@ class GamePage extends BaseComponent {
   async render() {
     await this._attachCSS(import.meta.url); 
     this.shadowRoot.innerHTML += `
-      <h1>Game Page</h1>
+      <div class="game-container">
+        <div class="breadcrumb-container">
+          <div class="image-bread-container">
+            <img class="home-icon" src="/assets/icons/common/Home.svg" alt="Home Icon"></img>
+            <h1 class="breadcrumb">Home>>Tabletop>>Peg Soltaire</h1>
+          </div>
+        </div>
+        <div class="game-area-container">
+          <div class="game-area">
+            <button class="start-button"><img src="/assets/images/Joystick.png" alt= "Joystick"></img></button>
+          </div>
+        </div>
+        <h1 class="game-name">Peg Soltaire</h1>
+
+      </div>
     `;
   }
 }
